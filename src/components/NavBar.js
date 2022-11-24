9ce8ea619ba9402ce76e0bc973e0b23b92cf83e1
Stan <skukhniy@gmail.com>
2022-11-24 09:16:30 -0500

    removed old navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,24 +3,10 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import SidebarMenu from 'react-bootstrap-sidebar-menu';
 
 function NavBar() {
-  return (
-    <Navbar className="navbar-custom" variant="light" expand="xl" fixed="top">
-      <Container>
-        <Navbar.Brand>Stanislav Kukhniy </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link href="#aboutMe">About Me</Nav.Link>
-            <Nav.Link href="#skills">Skills</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+  return <div>Sidebar</div>;
 }
 
 export default NavBar;
