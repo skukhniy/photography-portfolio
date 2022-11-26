dcfd5209f849ce52864be536e5450c90da4d5dd2
Stan <skukhniy@gmail.com>
2022-11-25 22:17:13 -0500

    added new scss for sidebar and changed app display to flex

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import MainDisplay from './components/MainDisplay';
-import NavBar from './components/NavBar';
+import NavBar from './components/Sidebar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/App.scss';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <Sidebar />
       <MainDisplay />
     </div>
   );
