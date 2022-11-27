ff34989267e1f6df3d8f95c7655c7b80af33825e
Stan <skukhniy@gmail.com>
2022-11-26 23:14:57 -0500

    react package was a silly idea, reverted back to my own component and added base a tags and photo

--- src/components/NavSidebar.js
+++ src/components/NavSidebar.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import me from '../assets/me.jpg';
 import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 
 function NavSidebar() {
   return (
-    <div className="">
-      <Sidebar>
-        <Menu>
-          <SubMenu label="Charts">
-            <MenuItem> Pie charts </MenuItem>
-            <MenuItem> Line charts </MenuItem>
-          </SubMenu>
-          <MenuItem> Documentation </MenuItem>
-          <MenuItem> Calendar </MenuItem>
-        </Menu>
-      </Sidebar>
+    <div className="sidebars">
+      <div className="header">
+        <img src={me}></img>
+        <h5>Slavography</h5>
+      </div>
+      <ul className="nav-items">
+        <li>
+          <a>Example 1</a>
+        </li>
+        <li>
+          <a>Example 2</a>
+        </li>
+        <li>
+          <a>Example 3</a>
+        </li>
+      </ul>
     </div>
   );
 }
